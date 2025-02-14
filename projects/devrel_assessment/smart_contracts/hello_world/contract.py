c03661fa78300f15f19ee656c24c4ceffd51f9eb
JBScaled <jorrin.taiko@gmail.com>
2025-02-14 05:51:00 -0500

    feat: set & get messages with BoxMap

--- projects/devrel_assessment/smart_contracts/hello_world/contract.py
+++ projects/devrel_assessment/smart_contracts/hello_world/contract.py
@@ -1,8 +1,27 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, String, BoxMap, arc4
 from algopy.arc4 import abimethod
-
+class Message(arc4.Struct):
+    message: arc4.String
 
 class HelloWorld(ARC4Contract):
+    
+    # Initialize the contract with a box to store the greeting message
+    def __init__(self) -> None:
+        # Create a BoxMap to store a collection of greetings, stored under a key the caller chooses
+        self.message_boxes = BoxMap(String, Message, key_prefix="")
+    
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def hello(self, message: String, box_name: String) -> arc4.String:
+        # Create and format the greeting message
+        message_to_add = arc4.String("Hello, " + message)
+        
+        # Store the message in the BoxMap
+        self.message_boxes[box_name] = Message(message = message_to_add)
+        
+        # Return the greeting message
+        return message_to_add
+
+    @abimethod()
+    def getMessage(self, box_name: String ) -> Message:        
+        # Return the stored message associated with the key provided
+        return self.message_boxes[box_name]
--- projects/devrel_assessment/smart_contracts/hello_world/deploy_config.py
+++ projects/devrel_assessment/smart_contracts/hello_world/deploy_config.py
@@ -1,11 +1,13 @@
 import logging
 
 import algokit_utils
+from algokit_utils import (
+    TransactionParameters,
+)
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
 logger = logging.getLogger(__name__)
-
 
 # define deployment behaviour based on supplied app spec
 def deploy(
@@ -28,9 +30,13 @@ def deploy(
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
     )
-    name = "world"
-    response = app_client.hello(name=name)
+    message = "Jorrin! Welcome to the wonderful world of Algorand"
+
+    box_name = "Success"
+    
+    #call the hello method and pass in the neccessary Box reference 
+    response = app_client.hello(message=message, box_name=box_name, transaction_parameters=TransactionParameters(boxes=[(1018, box_name)]))
     logger.info(
         f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.return_value}"
+        f"with name={message}, received: {response.return_value}"
     )
